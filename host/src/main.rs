34b7f7378d86c6f5cd9e35fea31b94bf8183822b
shreyas-londhe <shreyas_londhe@outlook.com>
2024-12-13 13:25:19 +0530

    refactor: removed duplicate code in host

--- host/src/main.rs
+++ host/src/main.rs
@@ -6,77 +6,78 @@ use methods::{
     EMAIL_VERIFY_ELF, EMAIL_VERIFY_ID, EMAIL_WITH_REGEX_VERIFY_ELF, EMAIL_WITH_REGEX_VERIFY_ID,
 };
 use risc0_zkvm::{default_prover, ExecutorEnv, Prover};
-use std::{env, path::PathBuf};
+use serde::{de::DeserializeOwned, Serialize};
+use std::{env, fmt::Debug, path::PathBuf};
 use utils::{generate_email_inputs, generate_email_with_regex_inputs};
 use zkemail_core::{Email, EmailVerifierOutput, EmailWithRegex, EmailWithRegexVerifierOutput};
 
-fn generate_and_verify_email_proof(prover: &dyn Prover, email: Email) -> Result<()> {
+enum VerificationType {
+    Signature,
+    SignatureWithRegex(PathBuf),
+}
+
+fn generate_and_verify_proof<T, U>(
+    prover: &dyn Prover,
+    input_data: T,
+    elf: &[u8],
+    verify_id: &[u32; 8],
+) -> Result<U>
+where
+    T: Serialize,
+    U: DeserializeOwned + Debug,
+{
     debug!("Starting ZK proof generation");
 
-    let input = postcard::to_allocvec(&email).unwrap();
+    let input = postcard::to_allocvec(&input_data).unwrap();
     let env = ExecutorEnv::builder()
         .write_frame(&input)
         .build()
         .map_err(|e| anyhow!("Failed to build environment: {}", e))?;
 
     let prove_info = prover
-        .prove(env, EMAIL_VERIFY_ELF)
+        .prove(env, elf)
         .map_err(|e| anyhow!("Failed to generate proof: {}", e))?;
 
     let receipt = prove_info.receipt;
-    let output: EmailVerifierOutput = receipt.journal.decode()?;
+    let output: U = receipt.journal.decode()?;
     println!("{:?}", output);
 
     receipt
-        .verify(EMAIL_VERIFY_ID)
+        .verify(*verify_id)
         .map_err(|e| anyhow!("Failed to verify proof: {}", e))?;
 
     info!("ZK proof generated and verified successfully");
-    Ok(())
+    Ok(output)
 }
 
-fn generate_and_verify_email_with_regex_proof(
-    prover: &dyn Prover,
-    email_with_regex: EmailWithRegex,
-) -> Result<()> {
-    let input = postcard::to_allocvec(&email_with_regex).unwrap();
-    let env = ExecutorEnv::builder()
-        .write_frame(&input)
-        .build()
-        .map_err(|e| anyhow!("Failed to build environment: {}", e))?;
-
-    let prove_info = prover
-        .prove(env, EMAIL_WITH_REGEX_VERIFY_ELF)
-        .map_err(|e| anyhow!("Failed to generate proof: {}", e))?;
-
-    let receipt = prove_info.receipt;
-    let output: EmailWithRegexVerifierOutput = receipt.journal.decode()?;
-    println!("{:?}", output);
-
-    receipt
-        .verify(EMAIL_WITH_REGEX_VERIFY_ID)
-        .map_err(|e| anyhow!("Failed to verify proof: {}", e))?;
-
-    info!("ZK proof generated and verified successfully");
-    Ok(())
-}
-
-async fn verify_email(from_domain: &str, email_path: &PathBuf) -> Result<()> {
-    let prover = default_prover();
-    let email_inputs = generate_email_inputs(from_domain, email_path).await?;
-    generate_and_verify_email_proof(prover.as_ref(), email_inputs)?;
-    Ok(())
-}
-
-async fn verify_email_with_regex(
+async fn verify_email(
     from_domain: &str,
     email_path: &PathBuf,
-    config_path: &PathBuf,
+    verification_type: VerificationType,
 ) -> Result<()> {
     let prover = default_prover();
-    let email_with_regex =
-        generate_email_with_regex_inputs(from_domain, email_path, config_path).await?;
-    generate_and_verify_email_with_regex_proof(prover.as_ref(), email_with_regex)?;
+
+    match verification_type {
+        VerificationType::Signature => {
+            let email_inputs = generate_email_inputs(from_domain, email_path).await?;
+            generate_and_verify_proof::<Email, EmailVerifierOutput>(
+                prover.as_ref(),
+                email_inputs,
+                EMAIL_VERIFY_ELF,
+                &EMAIL_VERIFY_ID,
+            )?;
+        }
+        VerificationType::SignatureWithRegex(config_path) => {
+            let email_with_regex_inputs =
+                generate_email_with_regex_inputs(from_domain, email_path, &config_path).await?;
+            generate_and_verify_proof::<EmailWithRegex, EmailWithRegexVerifierOutput>(
+                prover.as_ref(),
+                email_with_regex_inputs,
+                EMAIL_WITH_REGEX_VERIFY_ELF,
+                &EMAIL_WITH_REGEX_VERIFY_ID,
+            )?;
+        }
+    }
     Ok(())
 }
 
@@ -110,11 +111,16 @@ async fn main() -> Result<()> {
                 "Starting email verification with regex config from {:?}",
                 config
             );
-            verify_email_with_regex(from_domain, &email_path, &config).await?;
+            verify_email(
+                from_domain,
+                &email_path,
+                VerificationType::SignatureWithRegex(config),
+            )
+            .await?;
         }
         None => {
-            info!("Starting basic email verification without regex config");
-            verify_email(from_domain, &email_path).await?;
+            info!("Starting Signature email verification without regex config");
+            verify_email(from_domain, &email_path, VerificationType::Signature).await?;
         }
     }
 
